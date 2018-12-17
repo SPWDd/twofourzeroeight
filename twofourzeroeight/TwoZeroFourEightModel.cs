15b70534909905c89479e491facebff31129841c
SPWDd <42263163+SPWDd@users.noreply.github.com>
2018-12-17 19:33:22 +0700

    Add files via upload

---
+++ twofourzeroeight/TwoZeroFourEightModel.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twozerofoureight
+{
+    class TwoZeroFourEightModel : Model
+    {
+        protected int boardSize; // default is 4
+        protected int[,] board;
+        protected Random rand;
+        protected int[] range;
+
+        public TwoZeroFourEightModel() : this(4)
+        {
+            // default board size is 4 
+        }
+
+        public TwoZeroFourEightModel(int size)
+        {
+            boardSize = size;
+            board = new int[boardSize, boardSize];
+            range = Enumerable.Range(0, boardSize).ToArray();
+            foreach (int i in range)
+            {
+                foreach (int j in range)
+                {
+                    board[i, j] = 0;
+                }
+            }
+            rand = new Random();
+            // initialize board
+            HandleChanges();
+        }
+
+        public bool checkGameWin()
+        {
+            bool status = false;
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (board[i, j] == 2048)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        status = false;
+                    }
+                }
+            }
+            return status;
+        }
+
+        public bool checkGameOver()
+        {
+            int count = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (board[i, j] > 0)
+                    {
+                        count++;
+                    }
+                    if (i == 0 && j == 0)
+                    {
+                        if (board[i, j] == board[i + 1, j] || board[i, j] == board[i, j + 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i == 0 && j != 0 && j != 3)
+                    {
+                        if (board[i, j] == board[i + 1, j] || board[i, j] == board[i, j + 1] || board[i, j] == board[i, j - 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i == 0 && j == 3)
+                    {
+                        if (board[i, j] == board[i + 1, j] || board[i, j] == board[i, j - 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i != 0 && i != 3 && j == 0)
+                    {
+                        if (board[i, j] == board[i + 1, j] || board[i, j] == board[i - 1, j] || board[i, j] == board[i, j + 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i != 0 && i != 3 && j != 0 && j != 3)
+                    {
+                        if (board[i, j] == board[i + 1, j] || board[i, j] == board[i - 1, j] || board[i, j] == board[i, j + 1] || board[i, j] == board[i, j - 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i != 0 && i != 3 && j == 3)
+                    {
+                        if (board[i, j] == board[i + 1, j] || board[i, j] == board[i - 1, j] || board[i, j] == board[i, j - 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i == 3 && j == 0)
+                    {
+                        if (board[i, j] == board[i - 1, j] || board[i, j] == board[i, j + 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i == 3 && j != 0 && j != 3)
+                    {
+                        if (board[i, j] == board[i - 1, j] || board[i, j] == board[i, j + 1] || board[i, j] == board[i, j - 1])
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i == 3 && j == 3)
+                    {
+                        if (board[i, j] == board[i - 1, j] || board[i, j] == board[i, j - 1])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            if (count != 16)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public int GetScore()
+        {
+            int sum = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    sum = sum + board[i, j];
+                }
+            }
+            return sum;
+        }
+
+        public int[,] GetBoard()
+        {
+            return board;
+        }
+
+        private void AddRandomSlot()
+        {
+            while (true)
+            {
+                int x = rand.Next(boardSize);
+                int y = rand.Next(boardSize);
+                if (board[x, y] == 0)
+                {
+                    board[x, y] = 2;
+                    return;
+                }
+            }
+        }
+
+        // Perform shift and merge to the left of the given array.
+        protected bool ShiftAndMerge(int[] buffer)
+        {
+            bool changed = false; // whether the array has changed
+            int pos = 0; // next available slot index
+            int lastMergedSlot = -1; // last slot that resulted from merging
+            foreach (int k in range)
+            {
+                if (buffer[k] != 0) // nonempty slot
+                {
+                    // check if we can merge with the previous slot
+                    if (pos > 0 && pos - 1 > lastMergedSlot && buffer[pos - 1] == buffer[k])
+                    {
+                        // merge
+                        buffer[pos - 1] *= 2;
+                        buffer[k] = 0;
+                        lastMergedSlot = pos - 1;
+                        changed = true;
+                    }
+                    else
+                    {
+                        // shift to the next available slot
+                        buffer[pos] = buffer[k];
+                        if (pos != k)
+                        {
+                            buffer[k] = 0;
+                            changed = true;
+                        }
+                        // move the next available slot
+                        pos++;
+                    }
+                }
+            }
+            return changed;
+        }
+
+        protected void HandleChanges(bool changed = true)
+        {
+            // if the board has changed, add a new number
+            // and notify all views
+            if (changed)
+            {
+                AddRandomSlot();
+                NotifyAll();
+            }
+        }
+
+        public void PerformDown()
+        {
+            bool changed = false; // whether the board has changed
+            foreach (int i in range)
+            {
+                int[] buffer = new int[boardSize];
+                // extract the current column from bottom to top
+                foreach (int j in range)
+                {
+                    buffer[boardSize - j - 1] = board[j, i];
+                }
+                // process the extracted array
+                // also track changes
+                changed = ShiftAndMerge(buffer) || changed;
+                // copy back
+                foreach (int j in range)
+                {
+                    board[j, i] = buffer[boardSize - j - 1];
+                }
+            }
+            HandleChanges(changed);
+        }
+
+        public void PerformUp()
+        {
+            bool changed = false; // whether the board has changed
+            foreach (int i in range)
+            {
+                int[] buffer = new int[boardSize];
+                // extract the current column from top to bottom
+                foreach (int j in range)
+                {
+                    buffer[j] = board[j, i];
+                }
+                // process the extracted array
+                // also track changes
+                changed = ShiftAndMerge(buffer) || changed;
+                // copy back
+                foreach (int j in range)
+                {
+                    board[j, i] = buffer[j];
+                }
+            }
+            HandleChanges(changed);
+        }
+
+        public void PerformRight()
+        {
+            bool changed = false; // whether the board has changed
+            foreach (int i in range)
+            {
+                int[] buffer = new int[boardSize];
+                // extract the current column from right to left
+                foreach (int j in range)
+                {
+                    buffer[boardSize - j - 1] = board[i, j];
+                }
+                // process the extracted array
+                // also track changes
+                changed = ShiftAndMerge(buffer) || changed;
+                // copy back
+                foreach (int j in range)
+                {
+                    board[i, j] = buffer[boardSize - j - 1];
+                }
+            }
+            HandleChanges(changed);
+        }
+
+        public void PerformLeft()
+        {
+            bool changed = false; // whether the board has changed
+            foreach (int i in range)
+            {
+                int[] buffer = new int[boardSize];
+                // extract the current column from left to right
+                foreach (int j in range)
+                {
+                    buffer[j] = board[i, j];
+                }
+                // process the extracted array
+                // also track changes
+                changed = ShiftAndMerge(buffer) || changed;
+                // copy back
+                foreach (int j in range)
+                {
+                    board[i, j] = buffer[j];
+                }
+            }
+            HandleChanges(changed);
+        }
+    }
+}
---
+++ twofourzeroeight/TwoZeroFourEightView.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightView : Form, View
+    {
+        Model model;
+        Controller controller;
+
+        public TwoZeroFourEightView()
+        {
+            InitializeComponent();
+            model = new TwoZeroFourEightModel();
+            model.AttachObserver(this);
+            controller = new TwoZeroFourEightController();
+            controller.AddModel(model);
+            controller.ActionPerformed(TwoZeroFourEightController.LEFT);
+        }
+
+        public void Notify(Model m)
+        {
+            UpdateBoard(((TwoZeroFourEightModel)m).GetBoard());
+            UpdateScore(((TwoZeroFourEightModel)m).GetScore());
+            UpdateStatusWin(((TwoZeroFourEightModel)m).checkGameWin());
+            UpdateStatusLose(((TwoZeroFourEightModel)m).checkGameOver());
+        }
+        private void UpdateStatusWin(bool status)
+        {
+            if (status == true)
+            {
+                lblGameStatus.Text = "You win!";
+                btnDown.Enabled = false;
+                btnUp.Enabled = false;
+                btnLeft.Enabled = false;
+                btnRight.Enabled = false;
+                KeyPreview = false;
+            }
+            else
+            {
+                lblGameStatus.Text = "";
+            }
+        }
+
+        private void UpdateStatusLose(bool status)
+        {
+            if (status == true)
+            {
+                lblGameStatus.Text = "Game over!";
+                btnDown.Enabled = false;
+                btnUp.Enabled = false;
+                btnLeft.Enabled = false;
+                btnRight.Enabled = false;
+                KeyPreview = false;
+            }
+        }
+
+        private void UpdateScore(int score)
+        {
+            lblscore.Text = Convert.ToString(score);
+        }
+
+        private void UpdateTile(Label l, int i)
+        {
+            if (i != 0)
+            {
+                l.Text = Convert.ToString(i);
+            }
+            else
+            {
+                l.Text = "";
+            }
+            switch (i)
+            {
+                case 0:
+                    l.BackColor = Color.Gray;
+                    break;
+                case 2:
+                    l.BackColor = Color.DarkGray;
+                    break;
+                case 4:
+                    l.BackColor = Color.Orange;
+                    break;
+                case 8:
+                    l.BackColor = Color.Red;
+                    break;
+                default:
+                    l.BackColor = Color.Green;
+                    break;
+            }
+        }
+        private void UpdateBoard(int[,] board)
+        {
+            UpdateTile(lbl00, board[0, 0]);
+            UpdateTile(lbl01, board[0, 1]);
+            UpdateTile(lbl02, board[0, 2]);
+            UpdateTile(lbl03, board[0, 3]);
+            UpdateTile(lbl10, board[1, 0]);
+            UpdateTile(lbl11, board[1, 1]);
+            UpdateTile(lbl12, board[1, 2]);
+            UpdateTile(lbl13, board[1, 3]);
+            UpdateTile(lbl20, board[2, 0]);
+            UpdateTile(lbl21, board[2, 1]);
+            UpdateTile(lbl22, board[2, 2]);
+            UpdateTile(lbl23, board[2, 3]);
+            UpdateTile(lbl30, board[3, 0]);
+            UpdateTile(lbl31, board[3, 1]);
+            UpdateTile(lbl32, board[3, 2]);
+            UpdateTile(lbl33, board[3, 3]);
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            controller.ActionPerformed(TwoZeroFourEightController.LEFT);
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            controller.ActionPerformed(TwoZeroFourEightController.RIGHT);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            controller.ActionPerformed(TwoZeroFourEightController.UP);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            controller.ActionPerformed(TwoZeroFourEightController.DOWN);
+        }
+
+        private void TwoZeroFourEightView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lblGameStatus.Text == "")
+            {
+                switch (e.KeyData)
+                {
+                    case Keys.Up:
+                    case Keys.W:
+                        controller.ActionPerformed(TwoZeroFourEightController.UP);
+                        break;
+                    case Keys.Down:
+                    case Keys.S:
+                        controller.ActionPerformed(TwoZeroFourEightController.DOWN);
+                        break;
+                    case Keys.Left:
+                    case Keys.A:
+                        controller.ActionPerformed(TwoZeroFourEightController.LEFT);
+                        break;
+                    case Keys.Right:
+                    case Keys.D:
+                        controller.ActionPerformed(TwoZeroFourEightController.RIGHT);
+                        break;
+                }
+            }
+        }
+
+        private void TwoZeroFourEightView_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                    e.IsInputKey = true;
+                    break;
+            }
+        }
+
+        private void btnUp_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            TwoZeroFourEightView_PreviewKeyDown(sender, e);
+        }
+
+        private void btnDown_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            TwoZeroFourEightView_PreviewKeyDown(sender, e);
+        }
+
+        private void btnLeft_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            TwoZeroFourEightView_PreviewKeyDown(sender, e);
+        }
+
+        private void btnRight_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            TwoZeroFourEightView_PreviewKeyDown(sender, e);
+        }
+    }
+}
